4289d24f519f3ac3d4815de26a8d6e503cd5c461
Thibaut <thibaut.guibert@edu.escp.eu>
2021-02-03 16:57:56 +0100

    routes gerant les types

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const exampleRoutes = require("./example.routes");
+const typeRoutes = require("../Thibaut/routes/types.routes")
 
+router.use("/types", typeRoutes);
 router.use("/example", exampleRoutes);
 
 module.exports = router;
