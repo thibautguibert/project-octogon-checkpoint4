9b4b1bd40b957db2bc3732a39529ac5816cb4a14
Thibaut <thibaut.guibert@edu.escp.eu>
2021-02-04 20:18:31 +0100

    eslintcache dans le gitignore

--- front/src/Thibaut/CreateType.jsx
+++ front/src/Thibaut/CreateType.jsx
@@ -1,33 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./admin-forms.css"
 
 export const CreateType = () => {
+    const [newType, setNewType] = useState({
+        name: "",
+        logo: "",
+        color: ""
+    })
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewType({ ...newType, [name]: value })
+    }
     return (
         <main className="page">
             <section className="create-form">
-                <h1>Créer un rappeur</h1>
+                <h1>Créer un type</h1>
                 <form action="submit">
                     <label>
-                        <input type="text" name="nom" id="nom" placeholder="Nom" />
+                        <input type="text" name="name" value={newType.name} placeholder="Nom" onChange={handleChange} />
                     </label>
                     <label>
-                        <select name="type" id="type">
-                            <option value="" disabled selected hidden>Type</option>
-                        </select>
-                    </label>
-                    <label>
-                        <select name="rareté" id="rareté">
-                            <option value="" disabled selected hidden>Rareté</option>
-                        </select>
-                    </label>
-                    <label>
-                        <input type="number" name="puissance" id="puissance" placeholder="Puissance" />
-                    </label>
-                    <label>
-                        <input type="number" name="points-de-vie" id="points-de-vie" placeholder="Points de vie" />
-                    </label>
-                    <label>
-                        <input type="number" name="cout" id="cout" placeholder="Coût pour attaquer" />
+                        <input type="text" name="color" maxlength="6" value={newType.color} placeholder="Couleur (hexadecimal)" onChange={handleChange} />
                     </label>
                     <button type="submit" className="buttonCreate">Créer</button>
                 </form>
