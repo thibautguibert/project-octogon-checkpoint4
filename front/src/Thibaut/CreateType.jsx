fad06b03f486857035ecffbd57126b9946994c23
Thibaut <thibaut.guibert@edu.escp.eu>
2021-02-04 20:59:50 +0100

    formulaire creation de type pret sauf upload

--- front/src/Thibaut/CreateType.jsx
+++ front/src/Thibaut/CreateType.jsx
@@ -12,18 +12,25 @@ export const CreateType = () => {
         setNewType({ ...newType, [name]: value })
     }
     return (
-        <main className="page">
+        <main className="page pageFormulaire">
             <section className="create-form">
                 <h1>Créer un type</h1>
                 <form action="submit">
                     <label>
-                        <input type="text" name="name" value={newType.name} placeholder="Nom" onChange={handleChange} />
+                        <input type="text" name="name" value={newType.name} placeholder="Nom" onChange={handleChange} maxlength="14" />
                     </label>
                     <label>
                         <input type="text" name="color" maxlength="6" value={newType.color} placeholder="Couleur (hexadecimal)" onChange={handleChange} />
                     </label>
                     <button type="submit" className="buttonCreate">Créer</button>
                 </form>
+            </section>
+            <section className="previewTypeCreation" >
+                <div className="typeCard" style={newType.color.length === 6 ? { backgroundColor: `#${newType.color}` } : { backgroundColor: "#E6ECF2" }}>
+                    <div className="logoBackground">
+                        <p>{newType.name}</p>
+                    </div>
+                </div>
             </section>
         </main>
     )
