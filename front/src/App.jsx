5aa8894f14bc596a15329c4a23850d303cb125c1
Thibaut <thibaut.guibert@edu.escp.eu>
2021-02-04 10:33:01 +0100

    création du système de navigation. navbar desktop et mobile fonctionnelle

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import { Home } from './Thibaut/Home';
+import { Rules } from './Thibaut/Rules';
+import { Rappers } from './Thibaut/Rappers';
+import { TypesTable } from './Thibaut/TypesTable';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/rules" component={Rules} />
+        <Route path="/rappers" component={Rappers} />
+        <Route path="/types" component={TypesTable} />
+      </Switch>
+    </>
   );
 }
 
---
+++ front/src/Thibaut/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar } from './Navbar'
+import "./home.css"
+
+export const Home = () => {
+    return (
+        <main className="page">
+            <header>
+                <Navbar />
+            </header>
+        </main>
+    )
+}
